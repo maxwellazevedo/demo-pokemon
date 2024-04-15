f37b68c3a6576b47c1f580752d34e01f807f4435
Maxwell <maxwell.azevedo@dce.ufpb.br>
2024-04-15 16:54:43 -0300

    consumidor criado

--- src/main/kotlin/com/example/demopokemon/dataprovider/kafka/consumer/PokeConsumer.kt
+++ src/main/kotlin/com/example/demopokemon/dataprovider/kafka/consumer/PokeConsumer.kt
@@ -1,27 +1,32 @@
 package com.example.demopokemon.dataprovider.kafka.consumer
 
+import com.example.demopokemon.entity.PokeResponseEntity
 import lombok.extern.slf4j.Slf4j
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.slf4j.LoggerFactory
+import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.stereotype.Component
 
 @Slf4j
 @Component
 class PokeConsumer {
 
-//    private val LOGGER: Logger = LoggerFactory.getLogger(KafkaConsumer::class.java)
-//
-//    private var payload: PokeResponse? = null
-//
-//    @KafkaListener(topics = ["\${topic}"], groupId = "my_group_id")
-//    fun getMessage(consumerRecord: ConsumerRecord<*, *>) {
-//        LOGGER.info("Consumer message ='{}'", consumerRecord.toString())
-//        setPayload(consumerRecord.toString())
-//    }
-//
-//    private fun setPayload(payload: PokeResponse) {
-//        this.payload = payload
-//    }
-//
-//    fun getPayload(): String? {
-//        return payload
-//    }
+    private val LOGGER = LoggerFactory.getLogger(PokeConsumer::class.java)
+
+    private var payload: PokeResponseEntity? = null
+
+    @KafkaListener(topics = ["\${topic}"], groupId = "my_group_id")
+    fun getMessage(consumerRecord: ConsumerRecord<String, PokeResponseEntity>) {
+        LOGGER.info("m=getMessage, i=consumer_pokemon='{}'", consumerRecord.value())
+        setPayload(consumerRecord.value())
+    }
+
+    private fun setPayload(payload: PokeResponseEntity?) {
+        this.payload = payload
+    }
+
+    fun getPayload(): PokeResponseEntity? {
+        return payload
+    }
+
 }
